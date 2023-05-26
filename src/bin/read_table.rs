2f2c12de716936ce419a4149d19fef9f15ec675f
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2023-05-26 10:13:09 -0700

    and we are up and running and reading the tbl files as csv with a | delimiter and no headers

---
+++ src/bin/read_table.rs
@@ -0,0 +1,33 @@
+use std::error::Error;
+
+use csv;
+
+/// Reads data from a file into a reader and prints all records.
+///
+/// # Error
+///
+/// If an error occurs, the error is returned to `main`.
+
+fn read_from_file(path: &str) -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::ReaderBuilder::new()
+        .delimiter(b'|')
+        .has_headers(false)
+        .from_path(path)?;
+
+    // `.records` return an iterator of the internal
+    // record structure
+    for result in reader.records() {
+        let record = result?;
+
+        println!("{:?}", record);
+    }
+
+    Ok(())
+}
+
+fn main() {
+    // If an error occurs print error
+    if let Err(e) = read_from_file("./data/region.tbl") {
+        eprintln!("{}", e);
+    }
+}
