4d3537e2249cca891436dd0ff435e26dcf4c79d1
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2023-05-29 19:36:59 -0700

    cc

--- src/bin/reduce_table.rs
+++ src/bin/reduce_table.rs
@@ -32,67 +32,6 @@ pub struct Rtconfig {
     output_path: PathBuf,
 }
 
-/*
-#[derive(Deserialize, clap::Parser, Debug, Clone)]
-#[clap(author, version, about, long_about = None)]
-pub struct Rtconfig {
-    /// IP and port to listen on
-    #[arg(long, default_value = "")]
-    #[serde(default = "listen_endpoint")]
-    pub listen_endpoint: String,
-
-    /// Hostname and port at which to find the leader
-    #[arg(long, default_value = "")]
-    #[serde(default = "advertise_endpoint")]
-    pub leader_endpoint: String,
-
-    /// Hostname and port at which other nodes can find this node
-    #[arg(long, default_value = "")]
-    #[serde(default = "advertise_endpoint")]
-    pub advertise_endpoint: String,
-
-    /// The path at which to store the Raft / write-ahead log
-    #[arg(long, default_value = "")]
-    #[serde(default = "wal_path")]
-    pub wal_path: String,
-
-    /// The URL at which to store deltalake snapshots
-    #[arg(long, default_value = "")]
-    #[serde(default = "snapshot_url")]
-    pub snapshot_url: String,
-
-    /// The minimum number of nodes in a Raft group
-    #[arg(long, default_value_t = 3)]
-    #[serde(default = "replication_factor")]
-    pub replication_factor: usize,
-
-    /// The number of bytes to allocate for a block. It will split if its data exceeds this threshold
-    #[arg(long, default_value_t = 4_000_000)]
-    #[serde(default = "block_size")]
-    pub block_size: usize,
-
-    /// The maximum number of rows in a block before it splits
-    #[arg(long, default_value_t = 4000)]
-    #[serde(default = "row_capacity")]
-    pub row_capacity: usize,
-
-    /// If set to true, an fsync syscall will be performed for every new Raft message
-    #[arg(long, default_value_t = true)]
-    #[serde(default = "fsync")]
-    pub fsync: bool,
-
-    /// If set to true, an fsync syscall will be performed for every new Raft message
-    #[arg(long, default_value_t = true)]
-    #[serde(default = "persist")]
-    pub persist: bool,
-
-    /// If set to true, use SxtDb Tablets instead of ArrowTables
-    #[arg(long, default_value_t = false)]
-    #[serde(default = "sxt_db")]
-    pub sxt_db: bool,
-}
-*/
-
 fn main() {
     // File hosts must exist in current path before this produces output
 
@@ -116,16 +55,11 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-/// Conver tbl (csv) file to parquet
 pub fn convert_tbl(
     input_path: &str,
     output_path: &str,
     row_capacity: usize,
 ) -> std::io::Result<()> {
-    //let input_root_path = Path::new(output_path);
-    //let output_root_path = Path::new(output_path);
-    //println!("{:? :?}", input_root_path, output_root_path);
-
     for table in TPCH_TABLES {
         let input_path = format!("{input_path}/{table}.tbl");
         println!("{:?}", input_path);
@@ -155,102 +89,3 @@ pub fn convert_tbl(
     }
     Ok(())
 }
-
-/*
-/// Conver tbl (csv) file to parquet
-pub async fn convert_tbl(
-    input_path: &str,
-    output_path: &str,
-    file_format: &str,
-    partitions: usize,
-    batch_size: usize,
-    compression: Compression,
-) -> Result<()> {
-    let output_root_path = Path::new(output_path);
-    for table in TPCH_TABLES {
-        let start = Instant::now();
-        let schema = get_tbl_tpch_table_schema(table);
-
-        let input_path = format!("{input_path}/{table}.tbl");
-        let options = CsvReadOptions::new()
-            .schema(&schema)
-            .has_header(false)
-            .delimiter(b'|')
-            .file_extension(".tbl");
-
-        let config = SessionConfig::new().with_batch_size(batch_size);
-        let ctx = SessionContext::with_config(config);
-
-        // build plan to read the TBL file
-        let mut csv = ctx.read_csv(&input_path, options).await?;
-
-        // Select all apart from the padding column
-        let selection = csv
-            .schema()
-            .fields()
-            .iter()
-            .take(schema.fields.len() - 1)
-            .map(|d| Expr::Column(d.qualified_column()))
-            .collect();
-
-        csv = csv.select(selection)?;
-        // optionally, repartition the file
-        if partitions > 1 {
-            csv = csv.repartition(Partitioning::RoundRobinBatch(partitions))?
-        }
-
-        // create the physical plan
-        let csv = csv.create_physical_plan().await?;
-
-        let output_path = output_root_path.join(table);
-        let output_path = output_path.to_str().unwrap().to_owned();
-
-        println!(
-            "Converting '{}' to {} files in directory '{}'",
-            &input_path, &file_format, &output_path
-        );
-        match file_format {
-            "csv" => ctx.write_csv(csv, output_path).await?,
-            "parquet" => {
-                let props = WriterProperties::builder()
-                    .set_compression(compression)
-                    .build();
-                ctx.write_parquet(csv, output_path, Some(props)).await?
-            }
-            other => {
-                return Err(DataFusionError::NotImplemented(format!(
-                    "Invalid output format: {other}"
-                )));
-            }
-        }
-        println!("Conversion completed in {} ms", start.elapsed().as_millis());
-    }
-
-    Ok(())
-}
-*/
-
-// This takes in a set of big files and reduces them down to a much smaller number of lines.
-// Input parameters include the maximum number of lines a file can have
-// Eventually this function will also take in an optional start line and end line
-// So you can save off the smaller subset of lines from the bigger file
-// All of these parameters are optional and one can go with the defaults
-
-/*
-pub fn reduce_tables(path: impl AsRef<str>) -> Result<()> {
-    let path = path.as_ref();
-    // create directory to contain the Parquet files (one per partition)
-    let fs_path = std::path::Path::new(path);
-    if let Err(e) = fs::create_dir(fs_path) {
-        return Err(DataFusionError::Execution(format!(
-            "Could not create directory {path}: {e:?}"
-        )));
-    }
-
-    let filename = format!("part-{i}.parquet");
-    let path = fs_path.join(filename);
-    let file = fs::File::create(path)?;
-
-    Ok(())
-}
-*/
